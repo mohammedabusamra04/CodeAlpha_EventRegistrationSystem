bfaa20f6f68ad8ec09066524750c62f9e270a62e
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-06-08 21:11:40 +0300

    feat: add User model with Sequelize ORM and soft delete support

---
+++ src/models/User.js
@@ -0,0 +1,43 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    role: {
+        type: DataTypes.ENUM('user', 'admin', 'organizer'),
+        defaultValue: 'user',
+    },
+    avatar: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+}, {
+    timestamps: true,
+    paranoid: true,
+});
+
+module.exports = User;
