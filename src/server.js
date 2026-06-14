ed377155fd865f0a4c0d12a46c85d39332badc7d
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-06-14 18:20:33 +0300

    fix: add sequelize sync and move app.listen inside database connection

--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
 const app = require('./app');
 const sequelize = require('./config/database');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 sequelize.authenticate()
     .then(() => {
         console.log('Database connected');
+        return sequelize.sync({ alter: true });
+    })
+    .then(() => {
+        console.log('Database synced');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
     })
     .catch((err) => {
         console.error('Unable to connect to the database:', err);
     });
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
