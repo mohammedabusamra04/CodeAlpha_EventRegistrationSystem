e11895dd1f929d4f901a3b535762c7da0453ac4c
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-06-20 00:32:27 +0300

    refactor: enforce cascade delete on event associations

--- src/models/index.js
+++ src/models/index.js
@@ -6,10 +6,10 @@ const Review = require('./Review');
 const Notification = require('./Notification');
 
 
-User.hasMany(Event, { foreignKey: 'organizerId', as: 'organizedEvents' });
-User.hasMany(Registration, { foreignKey: 'userId' });
-User.hasMany(Review, { foreignKey: 'userId' });
-User.hasMany(Notification, { foreignKey: 'userId' });
+User.hasMany(Event, { foreignKey: 'organizerId', as: 'organizedEvents', onDelete: 'CASCADE' });
+User.hasMany(Registration, { foreignKey: 'userId', onDelete: 'CASCADE' });
+User.hasMany(Review, { foreignKey: 'userId', onDelete: 'CASCADE' });
+User.hasMany(Notification, { foreignKey: 'userId', onDelete: 'CASCADE' });
 
 
 Category.hasMany(Event, { foreignKey: 'categoryId' });
@@ -17,9 +17,9 @@ Category.hasMany(Event, { foreignKey: 'categoryId' });
 
 Event.belongsTo(User, { foreignKey: 'organizerId', as: 'organizer' });
 Event.belongsTo(Category, { foreignKey: 'categoryId', as: 'category' });
-Event.hasMany(Registration, { foreignKey: 'eventId' });
-Event.hasMany(Review, { foreignKey: 'eventId' });
-Event.hasMany(Notification, { foreignKey: 'eventId' });
+Event.hasMany(Registration, { foreignKey: 'eventId', onDelete: 'CASCADE' });
+Event.hasMany(Review, { foreignKey: 'eventId', onDelete: 'CASCADE' });
+Event.hasMany(Notification, { foreignKey: 'eventId', onDelete: 'CASCADE' });
 
 Registration.belongsTo(User, { foreignKey: 'userId' });
 Registration.belongsTo(Event, { foreignKey: 'eventId' });
