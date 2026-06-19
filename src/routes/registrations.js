d97cdf665fb58905609cd0cd8dc8e1e09a8f5466
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-06-20 00:39:00 +0300

    feat: integrate validation checks into registration routes

--- src/routes/registrations.js
+++ src/routes/registrations.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { getUserRegistrations, registerForEvent, cancelRegistration } = require('../controllers/registrationController');
 const authMiddleware = require('../middleware/auth');
+const { validateParamId } = require('../middleware/validation');
 
-router.post('/:eventId', authMiddleware, registerForEvent);
+router.post('/:eventId', authMiddleware, validateParamId('eventId'), registerForEvent);
 router.get('/', authMiddleware, getUserRegistrations);
-router.delete('/:id', authMiddleware, cancelRegistration);
+router.delete('/:id', authMiddleware, validateParamId('id'), cancelRegistration);
 
 module.exports = router;
