77387c5a9bb00ae4b8033ea079e60f66a9c9a78d
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-06-20 18:20:24 +0300

    fix: update database configuration for neon

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,14 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
-);
+});
 
 module.exports = sequelize;
